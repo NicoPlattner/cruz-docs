06cd312fc1f96a04c4bd3d6edc9715ed7619bb3e
Nico <nico.plattner@tuwien.ac.at>
2023-07-25 12:06:24 +0200

    changed default theme to dark

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,6 +64,7 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      defaultMode: 'dark',
       navbar: {
         title: 'My Site',
         logo: {
