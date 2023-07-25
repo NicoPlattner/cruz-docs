bd099d2271dac24b7179b0a2a6fc49439798e78d
Nico <nico.plattner@tuwien.ac.at>
2023-07-25 11:46:54 +0200

    test commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'CRUZ Docs',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Testing the cd',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
